151cabd19e637626ad70297b8dbe9be8f7fc2087
alveflo <victor.alveflo@gmail.com>
2021-06-08 20:03:53 +0200

    Implement RSS parser
    This commit will implement a naive RSS,
    i.e. xml parser, for the rss feed response.

--- main.go
+++ main.go
@@ -3,8 +3,33 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"encoding/xml"
+	"html/template"
 	"net/http"
 )
+
+type Rss2 struct {
+	XMLName		xml.Name	`xml:"rss"`
+	Version		string		`xml:"version,attr"`
+	// Required
+	Title		string		`xml:"channel>title"`
+	Link		string		`xml:"channel>link"`
+	Description	string		`xml:"channel>description"`
+	// Optional
+	PubDate		string		`xml:"channel>pubDate"`
+	ItemList	[]Item		`xml:"channel>item"`
+}
+
+type Item struct {
+	// Required
+	Title		string		`xml:"title"`
+	Link		string		`xml:"link"`
+	Description	template.HTML	`xml:"description"`
+	// Optional
+	Content		template.HTML	`xml:"encoded"`
+	PubDate		string		`xml:"pubDate"`
+	Comments	string		`xml:"comments"`
+}
 
 func main() {
 	resp, err := http.Get("https://news.ycombinator.com/rss")
@@ -20,5 +45,14 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	log.Println(string(body))
+	rss := Rss2{}
+
+	err = xml.Unmarshal(body, &rss)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for i := 0; i < len(rss.ItemList); i++ {
+		log.Println(rss.ItemList[i].Title)
+	}
 }
